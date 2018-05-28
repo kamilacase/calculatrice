5805b287b595681607ce4824aa604efac29a6d9e
kamilacase <xakdxshooter@live.fr>
2018-05-28 17:31:38 +0200

    bug

--- calc.js
+++ calc.js
@@ -2,11 +2,33 @@ var listBtn = document.getElementsByClassName("btn");
 var affiche = document.getElementById("affiche");
 var reset = document.getElementById("reset");
 
+reset.addEventListener("click" ,function()
+    {affiche.value = ""});
 
 
 for (var i = 0; i < listBtn.length; i++) {
   listBtn[i].addEventListener("click", function() {
   affiche.value += this.value;
-});
-console.log(listBtn[i]);
+  if (this.value == "=" ) {
+      calc();
+  }
+
+
+
 }
+
+function calc(){
+    var calcul = affiche.value;
+    var operator = "";
+    var strNumber ="";
+    for (var i = 0; i < calcul.length; i++) {
+        if (operator == ""){
+            if (calcul[i] == "+" || calcul[i] == "-" || calcul[i] == "*" || calcul[i] == "/" || calcul[i] == "="){
+                operator = calcul[i];
+            } else {
+            strNumber = calcul[i];
+
+            }
+        }
+    }
+};
