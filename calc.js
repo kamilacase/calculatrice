3a184473ae7f4fd89b190343f8705e800f7fc254
facoulon <f.coulon48@laposte.net>
2018-05-28 16:33:00 +0200

    Loop For done

--- calc.js
+++ calc.js
@@ -1,1 +1,12 @@
+var listBtn = document.getElementsByClassName("btn");
+var affiche = document.getElementById("affiche");
+var reset = document.getElementById("reset");
 
+
+
+for (var i = 0; i < listBtn.length; i++) {
+  listBtn[i].addEventListener("click", function() {
+  affiche.value += this.value;
+});
+console.log(listBtn[i]);
+}
