9693a376ec77444b58ad2e21b9f2eeb80f0d63fe
kamilacase <xakdxshooter@live.fr>
2018-05-28 15:21:43 +0200

    ajout de tous les boutons

--- calc.js
+++ calc.js
@@ -7,6 +7,12 @@ var btn6 = document.getElementById('touche6');
 var btn7 = document.getElementById('touche7');
 var btn8 = document.getElementById('touche8');
 var btn9 = document.getElementById('touche9');
+var btn0 = document.getElementById('touche0');
+var btnPlus = document.getElementById('touchePlus');
+var btnMoins = document.getElementById('toucheMoins');
+var btnMulti = document.getElementById('toucheMulti');
+var btnDiv = document.getElementById('toucheDiv');
+var btnEgal = document.getElementById('toucheEgal');
 
 
 var affiche = document.getElementById('affiche');
@@ -20,18 +26,48 @@ btn1.addEventListener("click", function() {
   affiche.value += '1';
 });
 
-btn1.addEventListener("click", function() {
+btn2.addEventListener("click", function() {
   affiche.value += '2';
 });
-btn1.addEventListener("click", function() {
-  affiche.value += '1';
-});
-btn1.addEventListener("click", function() {
+btn3.addEventListener("click", function() {
   affiche.value += '3';
 });
-btn1.addEventListener("click", function() {
-  affiche.value += '1';
+btn4.addEventListener("click", function() {
+  affiche.value += '4';
 });
-btn1.addEventListener("click", function() {
-  affiche.value += '1';
+btn5.addEventListener("click", function() {
+  affiche.value += '5';
 });
+btn6.addEventListener("click", function() {
+  affiche.value += '6';
+});
+btn7.addEventListener("click", function() {
+  affiche.value += '7';
+});
+btn8.addEventListener("click", function() {
+  affiche.value += '8';
+});
+btn9.addEventListener("click", function() {
+  affiche.value += '9';
+});
+btn0.addEventListener("click", function() {
+  affiche.value += '0';
+});
+btnPlus.addEventListener("click", function() {
+  affiche.value += '+';
+});
+btnMoins.addEventListener("click", function() {
+  affiche.value += '-';
+});
+btnMulti.addEventListener("click", function() {
+  affiche.value += '*';
+});
+btnDiv.addEventListener("click", function() {
+  affiche.value += '/';
+});
+btnEgal.addEventListener("click", function() {
+  affiche.value += '=';
+});
+if (btnPlus.addEventListener(click)) {
+
+}
